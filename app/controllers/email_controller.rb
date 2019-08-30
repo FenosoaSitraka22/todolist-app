29c1e7a9c61f4b46615439ff00ccb2940098f3f8
FenosoaSitraka22 <fenosoratsimba@gmail.com>
2019-08-30 16:03:46 +0300

    MAJ2

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -7,6 +7,7 @@ class EmailController < ApplicationController
 	def show
 		puts "*"*90
 	   puts @e = Email.find(params[:id])
+	   @e.update(read: true)
 		respond_to do |format|
 	       format.html{redirect_to root_path}    
 	       format.js {}
@@ -15,7 +16,7 @@ class EmailController < ApplicationController
 	end
 
 	def create
-		@e = Email.new(object: Faker::Book.title,body: Faker::Book.title )
+		@e = Email.new(object: Faker::Book.title,body: Faker::Book.title, read:false)
 	 if @e.save
 	   respond_to do |format|
 	       format.html{redirect_to root_path}    
@@ -29,12 +30,26 @@ class EmailController < ApplicationController
 	def destroy
 		@e =Email.find(params[:id])
 		@e.destroy
-		puts "/*"*95
+		
 		respond_to do |format|
 			format.html{redirect_to root_path}
 			format.js{}
 			flash[:notice]=" email deleted"
 		end 
+	end
+	
+	def update
+		puts params[:read]
+		@e =Email.find(params[:id])
+		if (@e.read == true)
+			@e.update(read: false)
+		else
+			@e.update(read: true)
+		end
+		respond_to do |format|
+	       format.html{redirect_to root_path}    
+	       format.js {}
+       	end	
 	end
 
 end
